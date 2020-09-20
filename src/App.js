47fec4a94f08b5b78579c938fb2ae004be66d16a
dona <bortouang01@gmail.com>
2020-09-20 20:20:48 +0100

    changes for v3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          BORTOUANG DONA ALEXIE
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          BORTOUANG DONA ALEXIE
+          Learn React
         </a>
       </header>
     </div>
